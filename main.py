07042aee45c2bbdcf370e560d92207184849782f
Arun RS <31178150+ARUNRS94@users.noreply.github.com>
2023-07-15 22:41:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,61 @@
+import os
+import sys
+import pdfplumber
 import openai
+from langchain.chains import ConversationalRetrievalChain, RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+from langchain.llms import OpenAI
+from langchain.vectorstores import Chroma
+import unstructured
 
-openai.api_key = '****'
+os.environ["OPENAI_API_KEY"] = '*****'
 
-with open('document.txt', 'r') as file:
-    document_text = file.read()
+PERSIST = False
 
-response = openai.Embedding.create(
-  input = document_text,
-  model = 'text-embedding-ada-002'
+pdf_file_path='data/Examplesoflegaltext_Legislatingforaccesstomedicines.pdf'
+
+def read_pdf(file_path):
+  with pdfplumber.open(file_path) as pdf:
+    document_text = ""
+    for page in pdf.pages:
+      document_text += page.extract_text()
+  return document_text
+
+pdf_text = read_pdf(pdf_file_path)
+
+query = None
+if len(sys.argv) > 1:
+  query = sys.argv[1]
+
+if PERSIST and os.path.exists("persist"):
+  print("Reusing index...\n")
+  vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
+  index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+else:
+  #loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
+  loader = DirectoryLoader("data/")
+  if PERSIST:
+    index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
+  else:
+    index = VectorstoreIndexCreator().from_loaders([loader])
+
+chain = ConversationalRetrievalChain.from_llm(
+  llm=ChatOpenAI(model="gpt-3.5-turbo"),
+  retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
 )
 
-vector = response['data'][0]['embedding']
+chat_history = []
+while True:
+  if not query:
+    query = input("Prompt: ")
+  if query in ['quit', 'q', 'exit']:
+    sys.exit()
+  result = chain({"question": query, "chat_history": chat_history})
+  print(result['answer'])
 
-print(vector)
+  chat_history.append((query, result['answer']))
+  query = None
